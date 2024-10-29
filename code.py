839e876827785fc805e5358659f94502aa1579b3
Morgan Aubert <morganaubert425@gmail.com>
2024-10-29 14:23:18 +0100

    V1.0 read csv

---
+++ code.py
@@ -0,0 +1,8 @@
+import numpy as numpy
+import matplotlib.pyplot as plt
+import pandas as pd
+from IPython.display import display
+
+
+exoplanetes=pd.read_csv('exoplanetes.csv',comment='#')
+display(exoplanetes)
