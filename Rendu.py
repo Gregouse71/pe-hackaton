ec022d277d41b2dc95d28b0e9379c85e616623a3
Lucas Duhautois <lucas.duhautois@etu.minesparis.psl.eu>
2024-10-29 16:59:19 +0100

    Ajout de commentaires dans Notebook Lucas

---
+++ Rendu.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+exoplanetes = pd.read_csv('exoplanetes.csv', comment='#')
+
+sns.set_theme(style="whitegrid")
+
+
