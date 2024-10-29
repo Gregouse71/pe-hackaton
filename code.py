08798f63f3f0ffb93a0f4d6ebcc98058a36c59b4
Morgan Aubert <morganaubert425@gmail.com>
2024-10-29 15:36:15 +0100

    V1.1 : def dict_to_mask

--- code.py
+++
@@ -1,8 +0,0 @@
-import numpy as numpy
-import matplotlib.pyplot as plt
-import pandas as pd
-from IPython.display import display
-
-
-exoplanetes=pd.read_csv('exoplanetes.csv',comment='#')
-display(exoplanetes)
